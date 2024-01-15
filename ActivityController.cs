f7353fc83fed650ad2dbce2361ff2f5bf18994c3
MattT-Plym <matthew.turley-1@students.plymouth.ac.uk>
2024-01-15 21:01:13 +0000

    Newer version, still unsure about connection.

---
+++ ActivityController.cs
@@ -0,0 +1,84 @@
+﻿// ActivityController.cs
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2001MicroService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class ActivityController : ControllerBase
+    {
+        private readonly EFDbContext _context;
+
+        public ActivityController(EFDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Activity
+        [HttpGet]
+        public IActionResult GetActivities()
+        {
+            var activities = _context.Activities.ToList();
+            return Ok(activities);
+        }
+
+        // GET: api/Activity/5
+        [HttpGet("{id}")]
+        public IActionResult GetActivity(int id)
+        {
+            var activity = _context.Activities.Find(id);
+
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(activity);
+        }
+
+        // POST: api/Activity
+        [HttpPost]
+        public IActionResult PostActivity(Activity activity)
+        {
+            _context.Activities.Add(activity);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetActivity), new { id = activity.ActivityID }, activity);
+        }
+
+        // PUT: api/Activity/5
+        [HttpPut("{id}")]
+        public IActionResult PutActivity(int id, Activity activity)
+        {
+            if (id != activity.ActivityID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(activity).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Activity/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteActivity(int id)
+        {
+            var activity = _context.Activities.Find(id);
+
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Activities.Remove(activity);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,35 +1,56 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+//using _2001MicroService.Models; // Adjust the namespace based on your project structure
 
 namespace _2001MicroService
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add Entity Framework Core with SQL Server
+            services.AddDbContext<EFDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Add controllers and configure JSON serialization
+            services.AddControllers().AddNewtonsoftJson();
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // Production error handling
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers(); // Map controllers for API endpoints
+
+                // Additional endpoints if needed
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
@@ -37,5 +58,4 @@ namespace _2001MicroService
             });
         }
     }
-    
 }
